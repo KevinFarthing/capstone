f5f8055cb0b6b3e3d911aa3ca22da7d8aa1a4900
KevinFarthing <farthing.kw@gmail.com>
2018-08-02 10:59:28 -0500

    added url functionality to sql/flask interaction

--- app/routes.py
+++ app/routes.py
@@ -10,4 +10,4 @@ def index():
     # user = {"don't need":"ain't gonna"}
     # yeah, actually. think i do need to run the connection(getstory) function here and assign it to a variable
     story = connect(get_story)
-    return render_template('index.html',prompt_author=story[1],story_author=story[2],prompt_text=story[3],story_text=story[4],uber_id=story[0])
+    return render_template('index.html',prompt_author=story[1],story_author=story[2],prompt_text=story[3],story_text=story[4],uber_id=story[0],prompt_url=story[5])
